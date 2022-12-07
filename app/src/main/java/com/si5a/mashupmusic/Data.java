d9563a98c67acfdb3a9f469c5db4825cc960d96e
nopendratrianto <jackiywijaya50@mhs.mdp.ac.id>
2022-12-07 17:09:30 +0700

    New Project

---
+++ app/src/main/java/com/si5a/mashupmusic/Data.java
@@ -0,0 +1,12 @@
+package com.si5a.mashupmusic;
+
+public class Data {
+
+    public static  String[][] data = new String[][] {
+            {"SING-OFF TIKTOK SONGS Part II (You Broke Me First, De Yang Gatal Gatal Sa) vs Mirriam Eka","Video ini adalah konten ber-copyright. sebagian penghasilan dari youtube akan masuk ke artist original pemilik lagu. Jika kalian mau memberikan support lebih berupa donasi pada kami untuk membantu memajukan channel ini, bisa klik link dibawah ini ya! Terima kasih banyak | https://saweria.co/rezadarmawangsa\n","X3wYCb2tACk"},
+            {"MASHUP OST Kartun/Anime 90an PART 1 - @EkaGustiwana & @Nadya_Rafika","Subscribe: http://bit.ly/SubscribeEka | Kita nostalgia ke tahun 90an yook! Tulis berapa umur kamu sekarang di kolom comment dan bagikan kenangan masa kecilmu waktu nonton","1Ixg_6Ldyu0"},
+            {"Pop Songs World 2020 - Mashup of 50+ Pop Songs","Pop Songs World 2020 - Mashup of 50+ Pop Songs | Video edit by Yabancı Müzikler | Music Mashed  by DJ Konsky.","2FZYSyvLqnY"},
+            {"28 MINUTES OF MASHUPS!","Hope everyone's doing well! We know staying home and social distancing isn't the easiest. So we've compiled some of your (and our) favourite mashups into one massive video to keep you guys entertained during these difficult times. Feel free to listen on repeat \uD83D\uDE09 Enjoy!","7FzLVQnKx9E"},
+    };
+
+}
